dfb9be50da92acda0bec6633469613b4ea60e2b0
Free Beachler <fbeachler@gmail.com>
2014-10-04 15:31:46 -0600

    updated license header in java files;

--- src/com/longevitysoft/android/appwidget/hstfeed/activity/HSTFeedWidgetInfo.java
+++ src/com/longevitysoft/android/appwidget/hstfeed/activity/HSTFeedWidgetInfo.java
@@ -5,15 +5,15 @@
  * HSTFeed is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
  * This code is distributed under the Creative Commons Non-Commercial License.
- * You are free to share and remix the code, but you must include
- * credit to the original author Free Beachler and a hyperlink to 
- * Longevity Software LLC (http://www.longevitysoft.com).  
- * You may not use this work for commercial purposes.  You agree to use this work 
- * in a manner that does not conflict with the HSTFeed Android Application.  If 
- * you alter, transform, or build upon this work, you may distribute the resulting 
- * work only under the same or newer version of this license.
+ * You are free to share and remix the code, but you must include credit to the 
+ * original author Free Beachler, Longevity Software LLC as described herein.  
+ * All distributions of this code and application must include the Longevity Software LLC logo.  
+ * Any remix and/or distribution of the application which is capable of displaying images must 
+ * also display the Longevity Software LLC logo with attribution.  You may not use this work 
+ * for commercial purposes.  You agree to use this work in a manner that does not conflict 
+ * with the HSTFeed Android Application.  If you alter, transform, or build upon this work, 
+ * you may distribute the resulting work only under the same or newer version of this license.
  * 
  * You should have received a copy of the Creative Commons Non-Commercial
  * License along with HSTFeed.  If not, see 
